e615f2a870f5831f72145c15dfcb55165daf910d
shsobur <shsoburhossen951@gmail.com>
2024-06-16 14:31:52 +0600

    complete hero baner section

--- src/layout/homePage/Baner/Baner.jsx
+++ src/layout/homePage/Baner/Baner.jsx
@@ -2,19 +2,38 @@ import gsap from "gsap";
 import {useGSAP} from "@gsap/react";
 import {heroVideo, smallHeroVideo} from "../../../utils";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const Baner = () => {
   const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo );
 
   useGSAP(() => {
-    gsap.to ("#hero-baner", {opacity: 1, delay: 2})
-  }, [])
+    gsap.to ("#baner-text-one", {opacity: 1, delay: 2})
+    gsap.to ("#iht", {opacity: 1, y: -40, delay: 2})
+  }, []);
+
+  const handelVideoSrc = () => {
+    if(window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    }
+    else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handelVideoSrc);
+
+    return () => {
+      window.removeEventListener("resize", handelVideoSrc);
+    }
+  }, []);
 
   return (
     <section className="w-full mt-10 bg-black nav-height relative">
-      <div className="h-5/6 w-full flex-cntainer flex-col">
+      <div className="h-auto w-full flex-cntainer flex-col">
 
-        <h2 id="hero-baner" className="hero-title">iPhone 15 Pro</h2>
+        <h2 id="baner-text-one" className="hero-title">iPhone 15 Pro</h2>
 
         <div className="w-9/12 md:w-10/12 m-auto">
           <video autoPlay loop muted playsInline={true}>
@@ -23,6 +42,12 @@ const Baner = () => {
         </div>
 
       </div>
+
+      <div id="iht" className="flex flex-col items-center opacity-0 translate-y-20 mt-10">
+        <a href="highlight" className="btn">Buy</a>
+        <h3 className="font-normal text-xl">From $199/mounth or $999</h3>
+      </div>
+
     </section>
   );
 };
